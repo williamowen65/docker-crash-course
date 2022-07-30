9ff862ff3801860d94a295e189cb65b259ca0ff4
William Owen <william.owen.bsc@gmail.com>
2022-07-29 20:34:56 -0700

    change

--- api/app.js
+++ api/app.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
   res.json([
     {
       "id": "1",
-      "title": "Book Reffviefffw: The Name of the Wind"
+      "title": "Book fdf: The Name of the Wind"
     },
     {
       "id": "2",
